a3c0ae60e9f95fb41ca0b6e2b435287970e28aa0
Marrione Nguyen <mtiamsic@gmail.com>
2020-12-12 16:33:54 -0800

    working on About

---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import defaultPhoto from '../../'
+
+
+function About() {
+    return (
+        <div>
+            <div>
+                <img className="defaultPhoto" src={defaultPhoto}></img>
+            </div>
+        </div>
+    )
+}
