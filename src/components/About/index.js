b2ac6fe3d995957ebc8554bbedf38fc9cb634fde
Rheine N <mtiamsic@gmail.com>
2021-01-20 16:03:13 -0800

    update About

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -13,7 +13,7 @@ function About() {
                     <h3 class="post-title">About Me</h3>
                     <div class="post-content">
                         <p>My name is Marrione Nguyen, my friends and family call me 'Rheine'.  I am a Technical Writer for compliance and product testing industry. As a Technical Writer (focused on Environmental Simulation Testing), I am responsible for reviewing and compiling data
-                            that was collected by the engineers.  During my years as a technical writer, I gained a wide-range of knowledge and understanding about complaince testing.  As a result, I am able to tell if the engineer correctly declared if a product is complaint or non-compliant.
+                            that was collected by the engineers.  During my years as a technical writer, I gained a wide-range of knowledge and understanding about compliance testing.  As a result, I am able to tell if the engineer correctly declared if a product is complaint or non-compliant.
                         </p>
                         <p>Currently, I am pursuing Full Stack Web Development at UC Berkeley Extension.  I am an aspiring web developer hoping to create a report generator that can take in massive amounts of data while being operated by multiple engineers.</p>
                     </div>
